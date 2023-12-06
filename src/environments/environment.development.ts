56893fe166afe4482e7cff52d470081a39369047
Akash Seewoosurran <aseew92@gmail.com>
2023-12-06 09:56:53 +0400

    Update environment files with development configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD9bFJhdI3Fl7paDCMqqpZMUsg-TR10U3g',
+    authDomain: 'pubgmesf.firebaseapp.com',
+    projectId: 'pubgmesf',
+    storageBucket: 'pubgmesf.appspot.com',
+    messagingSenderId: '1013840058297',
+    appId: '1:1013840058297:web:7ab641e33bc5bb752a0868',
+  },
+  apiUrl: 'http://192.168.100.236:10086',
+  localUrl: 'http://127.0.0.1:3002',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,3 @@
 export const environment = {
-    production: false,
-    firebaseConfig : {
-        apiKey: "AIzaSyD9bFJhdI3Fl7paDCMqqpZMUsg-TR10U3g",
-        authDomain: "pubgmesf.firebaseapp.com",
-        projectId: "pubgmesf",
-        storageBucket: "pubgmesf.appspot.com",
-        messagingSenderId: "1013840058297",
-        appId: "1:1013840058297:web:7ab641e33bc5bb752a0868"
-      }
+
 };
