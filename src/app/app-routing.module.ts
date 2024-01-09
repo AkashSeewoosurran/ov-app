14aa1716d6850209f7aa45a8fc6aace211bd8e5d
Akash Seewoosurran <aseew92@gmail.com>
2024-01-10 02:15:51 +0400

    Update routing paths in app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'ssoverlay',
+    path: 'ssov',
     loadChildren: () =>
       import('./component/ss-overlay/module/ss-overlay.module').then(
         (m) => m.SsOverlayModule
@@ -14,6 +14,35 @@ const routes: Routes = [
     loadChildren: () =>
       import('./component/dashboard/module/dashboard.module').then(
         (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: 'mapov',
+    loadChildren: () =>
+      import('./component/map-overlay/module/map-overlay.module').then(
+        (m) => m.MapOverlayModule
+      ),
+  },
+  {
+    path: 'stov',
+    loadChildren: () =>
+      import(
+        './component/standing-overlay/module/standing-overlay.module'
+      ).then((m) => m.StandingOverlayModule),
+  },
+
+  {
+    path: 'osov',
+    loadChildren: () =>
+      import(
+        './component/overrall-standing/module/overall-standing.module'
+      ).then((m) => m.OverallStandingModule),
+  },
+  {
+    path: 'caster-dashboard',
+    loadChildren: () =>
+      import('./component/casterDasboard/module/caster-dashboard.module').then(
+        (m) => m.CasterDashboardModule
       ),
   },
 ];
