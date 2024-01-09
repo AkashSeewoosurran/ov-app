ed77e0f46cc8bdc02ca80b7194eafc90e03378f1
Akash Seewoosurran <aseew92@gmail.com>
2024-01-10 02:19:45 +0400

    Update player-stats component styles and HTML

--- src/app/shared/component/player-stats/player-stats.component.ts
+++ src/app/shared/component/player-stats/player-stats.component.ts
@@ -4,6 +4,9 @@ import { PubgmDataService } from '../../services/pubgm-data.service';
 import { PlayerInfoList, mvpPlayer } from '../../models/playerInfo.model';
 import { AppCustomMaterialModule } from '../../modules/app-custom-material.module';
 import { LocalTeamInfo } from '../../models/teamInfo.model';
+import { forkJoin } from 'rxjs';
+import { MATCHES } from '../../models/matchStanding.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-player-stats',
@@ -14,25 +17,22 @@ import { LocalTeamInfo } from '../../models/teamInfo.model';
 })
 export class PlayerStatsComponent implements OnInit, AfterViewInit {
   playerInfoList$: PlayerInfoList[] = [];
-  mvpPlayer: mvpPlayer = {
-    uId: '',
-    playerName: '',
-    teamName: '',
-    damage: 0,
-    assists: 0,
-    survivalTime: 0,
-    inDamage: 0,
-    knockouts: 0,
-    killNum: 0,
-    rank: 0,
-    character: '',
-    teamLogo: '',
-  };
-  localTeamInfo: LocalTeamInfo[] = [];
+  mvpPlayer: mvpPlayer;
+  head2head: mvpPlayer[] = [];
+  top4GunslingersList: mvpPlayer[] = [];
 
-  constructor(private service: PubgmDataService) {}
+  MATCHES = MATCHES;
+  localTeamInfo: LocalTeamInfo[] = [];
+  playerPath: string = 'D:/webdev/pubgm/pubgm-app/src/';
+  casterDisabled: boolean = true;
+
+  constructor(private service: PubgmDataService, private router: Router) {}
 
   ngOnInit(): void {
+    if (this.router.url === '/caster-dashboard') {
+      this.casterDisabled = false;
+    }
+    console.log(this.head2head.length);
     this.service.getLocalTeamInfo().subscribe({
       next: (data) => {
         this.localTeamInfo = data;
@@ -40,12 +40,32 @@ export class PlayerStatsComponent implements OnInit, AfterViewInit {
     });
   }
 
+  // ngAfterViewInit(): void {
+  //   this.service.getPlayerInfoList().subscribe({
+  //     next: (data) => {
+  //       this.playerInfoList$ = data.playerInfoList;
+  //     },
+  //   });
+  // }
+
   ngAfterViewInit(): void {
     this.service.getPlayerInfoList().subscribe({
       next: (data) => {
         this.playerInfoList$ = data;
       },
     });
+  }
+
+  generatePlayerStats(): void {
+    this.getMvpPlayer();
+    this.getheadToHead();
+    this.gettop4Gunslingers();
+  }
+
+  savePlayerStats(): void {
+    console.log(this.head2head.length);
+    console.log(this.mvpPlayer);
+    console.log(this.top4GunslingersList.length);
   }
 
   getMvpPlayer(): void {
@@ -74,33 +94,50 @@ export class PlayerStatsComponent implements OnInit, AfterViewInit {
       teamName: highestStatsPlayer.teamName,
       damage: highestStatsPlayer.damage,
       assists: highestStatsPlayer.assists,
-      survivalTime: highestStatsPlayer.survivalTime / 60,
+      survivalTime:
+        Math.floor(highestStatsPlayer.survivalTime / 60) +
+        ':' +
+        (highestStatsPlayer.survivalTime % 60).toString().padStart(2, '0'),
       inDamage: highestStatsPlayer.inDamage,
       knockouts: highestStatsPlayer.knockouts,
       killNum: highestStatsPlayer.killNum,
       rank: highestStatsPlayer.rank,
-      character: highestStatsPlayer.character,
-      teamLogo: highestStatsPlayer.picUrl,
+      character: this.playerPath + highestStatsPlayer.character,
+      teamLogo: this.playerPath + highestStatsPlayer.picUrl,
     };
 
-    console.log(mvpPlayer);
-
-    this.headToHead();
-    this.top4Gunslingers();
     this.mvpPlayer = mvpPlayer;
   }
 
-  headToHead(): void {
+  getheadToHead(): void {
     const headToHead = this.playerInfoList$
       .sort((a, b) => {
         return b.killNum - a.killNum;
       })
+      .map((player) => {
+        return {
+          playerName: player.playerName,
+          killNum: player.killNum,
+          character: this.playerPath + player.character,
+          rank: player.rank,
+          teamName: player.teamName,
+          teamLogo: this.playerPath + player.picUrl,
+          knockouts: player.knockouts,
+          damage: player.damage,
+          inDamage: player.inDamage,
+          assists: player.assists,
+          survivalTime:
+            Math.floor(player.survivalTime / 60) +
+            ':' +
+            (player.survivalTime % 60).toString().padStart(2, '0'),
+        };
+      })
       .slice(0, 2);
 
-    console.log(headToHead);
+    this.head2head = headToHead as mvpPlayer[];
   }
 
-  top4Gunslingers(): void {
+  gettop4Gunslingers(): void {
     const top4Gunslingers = this.playerInfoList$
       .sort((a, b) => {
         return b.damage - a.damage;
@@ -110,14 +147,44 @@ export class PlayerStatsComponent implements OnInit, AfterViewInit {
           playerName: player.playerName,
           damage: player.damage,
           killNum: player.killNum,
-          character: player.character,
+          character: this.playerPath + player.character,
           rank: player.rank,
           teamName: player.teamName,
-          teamLogo: player.picUrl,
+          teamLogo: this.playerPath + player.picUrl,
         };
       })
       .slice(0, 4);
 
-    console.log(top4Gunslingers);
+    this.top4GunslingersList = top4Gunslingers as mvpPlayer[];
+  }
+
+  saveAllPlayerStats(): void {
+    if (
+      this.head2head.length > 0 &&
+      this.top4GunslingersList.length > 0 &&
+      this.mvpPlayer != null
+    ) {
+      this.service.saveAllPlayerData(
+        this.mvpPlayer,
+        this.head2head,
+        this.top4GunslingersList
+      );
+    }
+  }
+
+  onMatchSelected(matchId: number) {
+    if (matchId) {
+      forkJoin({
+        mvpPlayer: this.service.getMvpPlayer(matchId),
+        head2head: this.service.getHead2Head(matchId),
+        top4GunslingersList: this.service.getTop4GunSlingers(matchId),
+      }).subscribe({
+        next: ({ mvpPlayer, head2head, top4GunslingersList }) => {
+          this.mvpPlayer = mvpPlayer[0];
+          this.head2head = head2head;
+          this.top4GunslingersList = top4GunslingersList;
+        },
+      });
+    }
   }
 }
